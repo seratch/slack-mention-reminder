321020d3f9a8943b3505dd613ac0edad889bca0e
Kazuhiro Sera <seratch@gmail.com>
2024-01-26 07:49:11 +0900

    Refactor the code

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,23 @@ export const app = new SlackApp({
   env: {
     SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN!,
     SLACK_APP_TOKEN: process.env.SLACK_APP_TOKEN!,
-    SLACK_LOGGING_LEVEL: "DEBUG",
+    SLACK_LOGGING_LEVEL: "DEBUG", // "INFO" is recommended for production
   },
 });
 
-// @this-bot remind #tasks at 2024-01-25 09:00 +09:00 \n the message here
-// @this-bot remind #tasks at 1706180400 \n the message here
+// The format:
+// @MentionReminder remind [channel/user] at [datetime]\n[message]
+//
+// Examples:
+// @MentionReminder remind #tasks at 2024-01-25 09:00 +09:00 \n the message here
+// @MentionReminder remind #tasks at 1706180400 \n the message here
 const parser = /<@.+>\s+remind\s+<#?@?(\w+)\|?.*>\s+at\s+(.+)\n([^]+)$/;
 
 app.event("app_mention", async ({ payload, context }) => {
   const parsed = parser.exec(payload.text);
   let error: string | undefined;
   if (parsed) {
-    const [_, channelOrUser, time, text] = parsed;
+    const [_, channelOrUser, datetimeOrUnixTimestamp, text] = parsed;
     let channel = channelOrUser;
     if (channelOrUser.startsWith("U") || channelOrUser.startsWith("W")) {
       try {
@@ -30,27 +34,28 @@ app.event("app_mention", async ({ payload, context }) => {
         error = `Failed to start a DM with <@${channelOrUser}> due to ${code}`;
       }
     }
-    let validTime: string | number = time;
-    if (time.match(/^\d+$/)) {
-      validTime = Number.parseInt(time) * 1000;
+    let validTime: string | number = datetimeOrUnixTimestamp;
+    if (datetimeOrUnixTimestamp.match(/^\d+$/)) {
+      validTime = Number.parseInt(datetimeOrUnixTimestamp) * 1000;
     }
     const post_at = new Date(validTime).getTime() / 1000;
-    const now = new Date().getTime() / 1000;
     if (isNaN(post_at)) {
       error = `Failed to parse the time: ${validTime}`;
     }
+    const now = new Date().getTime() / 1000;
     if (post_at - now < 60 * 3) {
       error = `Scheduled too soon: ${validTime}`;
     }
     if (!error) {
       try {
         await context.client.chat.scheduleMessage({ channel, text, post_at });
-        return;
       } catch (e: unknown) {
         const code = (e as SlackAPIError).error;
         error = `Failed to schedule a message due to ${code}`;
       }
     }
+  } else {
+    error = "Failed to parse the input: " + "`" + payload.text + "`";
   }
   if (error) {
     await context.say({
--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,18 @@ import { app } from "./app";
 (async () => {
   const sm = new SocketModeClient({
     appToken: process.env.SLACK_APP_TOKEN!,
-    logLevel: LogLevel.DEBUG,
+    logLevel: LogLevel.DEBUG, // INFO is recommended for production
   });
-  sm.on("slack_event", async ({ body, ack, retry_num, retry_reason }) => {
-    const request = fromSocketModeToRequest({
-      body,
-      retryNum: retry_num,
-      retryReason: retry_reason,
-    });
-    if (!request) {
-      return;
-    }
-    const response = await app.run(request);
-    await ack(await fromResponseToSocketModePayload({ response }));
-  });
+  sm.on(
+    "slack_event",
+    async ({ body, ack, retry_num: retryNum, retry_reason: retryReason }) => {
+      const request = fromSocketModeToRequest({ body, retryNum, retryReason });
+      if (!request) {
+        return;
+      }
+      const response = await app.run(request);
+      await ack(await fromResponseToSocketModePayload({ response }));
+    },
+  );
   await sm.start();
 })();
